ffa1602e61e53d4151fe5dbd0335e4e1d07f77c2
rasmns <260301x@gmail.com>
2024-05-25 15:10:47 +0300

    (￣┰￣*)

--- My project (2)/Assets/scripts/InventoryItemBehavior.cs
+++ My project (2)/Assets/scripts/InventoryItemBehavior.cs
@@ -2,10 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class inventoryItemBehavior : ScriptableObject
+public enum InventoryItem
+{
+    //  koiraCurrency_btn,
+    //  piskiCollectible_btn,
+    //  currency,
+    //  weapon,
+    //  consumable,
+    //  ammo,
+    //  food,
+    //  questItem,
+    //  key,
+    //  powerUp,
+    //  collectible
+}
+
+public class InventoryItemBehavior : MonoBehaviour
 {
     public ItemType itemType;
     public string itemName;
     public string itemDescription;
-    public int itemStackSize;
+    public int itemStackCount;
+    public int itemCoords;
 }
